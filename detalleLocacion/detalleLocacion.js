1e3779647d8119110422aa591263e075fc9725b7
avizcarra <a20181454@pucp.edu.pe>
2022-06-21 19:58:41 -0500

    Corrección

--- detalleLocacion/detalleLocacion.js
+++ detalleLocacion/detalleLocacion.js
@@ -2,27 +2,4 @@ $(document).ready(function () {
     // Metodo de obtención de parámetros
     const urlParams = new URLSearchParams(window.location.search);
     const idLocacion = urlParams.get('locacion');
-    const idRegion = urlParams.get('region');
-    let urlLocacion = "https://pokeapi.co/api/v2/region/" + idRegion;
-
-    function llenarLocaciones() {
-        $.ajax({
-            method: "GET",
-            url: urlLocacion,
-        }).done(function (data) {
-            let listaLocalidades = data.locations;
-            let tblDinamic = "";
-            for (let i = 0; i < listaLocalidades.length; i++) {
-                tblDinamic += "<tr>";
-                tblDinamic += "   <td>" + (i + 1) + "</td>";
-                tblDinamic += "   <td>" + listaLocalidades[i].name + "</td>";
-                tblDinamic += "   <td>" +"<button type=\"button\" class=\"btn btn-primary\" onclick='' href="+listaLocalidades[i].url+" > Detalle </button>"+ "</td>";
-                tblDinamic += "</tr>";
-            }
-            console.log(tblDinamic);
-            $("tbody").html(tblDinamic);
-        }).fail(function (e) {
-            console.log(e);
-        });
-    }
 });
--- detalleRegion/detalleRegion.js
+++ detalleRegion/detalleRegion.js
@@ -3,5 +3,27 @@ $(document).ready(function () {
     // Metodo de obtención de parámetros
     const urlParams = new URLSearchParams(window.location.search);
     const idRegion = urlParams.get('region');
-
+    let urlLocacion = "https://pokeapi.co/api/v2/region/" + idRegion;
+    function llenarLocaciones() {
+        $.ajax({
+            method: "GET",
+            url: urlLocacion,
+        }).done(function (data) {
+            let listaLocalidades = data.locations;
+            let tblDinamic = "";
+            for (let i = 0; i < listaLocalidades.length; i++) {
+                tblDinamic += "<tr>";
+                tblDinamic += "   <td>" + (i + 1) + "</td>";
+                tblDinamic += "   <td>" + listaLocalidades[i].name + "</td>";
+                let id  = listaLocalidades[i].url.split("/",-1);
+                let redireccionLink = "./detalleLocacion/detalleLocacion?id="+id;
+                tblDinamic += "   <td>" +"<button type=\"button\" class=\"btn btn-primary\" onclick='' href="+redireccionLink+" > Detalle </button>"+ "</td>";
+                tblDinamic += "</tr>";
+            }
+            console.log(tblDinamic);
+            $("tbody").html(tblDinamic);
+        }).fail(function (e) {
+            console.log(e);
+        });
+    }
 });
