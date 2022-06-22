b053c0465191b41eac0d1db6ea43372bd443c986
avizcarra <a20181454@pucp.edu.pe>
2022-06-21 20:26:54 -0500

    paginador

--- detalleRegion/detalleRegion.js
+++ detalleRegion/detalleRegion.js
@@ -4,26 +4,43 @@ $(document).ready(function () {
     const urlParams = new URLSearchParams(window.location.search);
     const idRegion = urlParams.get('region');
     let urlLocacion = "https://pokeapi.co/api/v2/region/" + idRegion;
+    var listaLocalidades;
+    var numpage;
     function llenarLocaciones() {
         $.ajax({
             method: "GET",
             url: urlLocacion,
         }).done(function (data) {
-            let listaLocalidades = data.locations;
+            listaLocalidades = data.locations;
             let tblDinamic = "";
-            for (let i = 0; i < listaLocalidades.length; i++) {
+            //para el calculo del numero de páginas
+            let numPerPage = 10;
+            if(listaLocalidades.length%numPerPage==0){
+                //quiere decir que no hay residuo
+                numpage=listaLocalidades.length/numPerPage;
+            }else{
+                numpage=~~(listaLocalidades.length/numPerPage)+1;
+            }
+            for (let i = 0; i < numPerPage; i++) {
                 tblDinamic += "<tr>";
                 tblDinamic += "   <td>" + (i + 1) + "</td>";
                 tblDinamic += "   <td>" + listaLocalidades[i].name + "</td>";
-                let id  = listaLocalidades[i].url.split("/",-1);
+                let id  = listaLocalidades[i].url.split("/").at(-2);
                 let redireccionLink = "./detalleLocacion/detalleLocacion?id="+id;
-                tblDinamic += "   <td>" +"<button type=\"button\" class=\"btn btn-primary\" onclick='' href="+redireccionLink+" > Detalle </button>"+ "</td>";
+                tblDinamic += "   <td>" +"<a type=\"button\" class=\"btn btn-primary\" href="+redireccionLink+" > Detalle </a>"+ "</td>";
                 tblDinamic += "</tr>";
             }
-            console.log(tblDinamic);
+            //console.log(tblDinamic);
             $("tbody").html(tblDinamic);
+            let paginator = "";
+            paginator = "<li class=\"page-item active\"><a value=\"1\" class=\"page-link\">1</a></li>"
+            for(let j=1; j < numpage;j++){
+                paginator += "<li class=\"page-item\" aria-current=\"page\"><span value="+j+1+" class=\"page-link\">+"+j+1+"+</span></li>";
+            }
+            $("li").html(paginator);
         }).fail(function (e) {
             console.log(e);
         });
     }
+
 });
